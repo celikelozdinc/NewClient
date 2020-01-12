77cf48dec76bedbbe4660539c1524d6b4ffc8058
celikelozdinc <ozdinc.celikel@gmail.com>
2020-01-12 15:55:50 +0300

    deserialize context

--- src/main/java/tr/edu/itu/bbf/cloudcore/distributed/Application.java
+++ src/main/java/tr/edu/itu/bbf/cloudcore/distributed/Application.java
@@ -1,5 +1,7 @@
 package tr.edu.itu.bbf.cloudcore.distributed;
 
+import com.esotericsoftware.kryo.Kryo;
+import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,9 +9,20 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.messaging.MessageHeaders;
+import org.springframework.statemachine.StateMachineContext;
+import org.springframework.statemachine.kryo.MessageHeadersSerializer;
+import org.springframework.statemachine.kryo.StateMachineContextSerializer;
+import org.springframework.statemachine.kryo.UUIDSerializer;
+import tr.edu.itu.bbf.cloudcore.distributed.entity.Events;
+import tr.edu.itu.bbf.cloudcore.distributed.entity.States;
 import tr.edu.itu.bbf.cloudcore.distributed.service.StateMachineWorker;
 
+import java.io.ByteArrayInputStream;
 import java.net.UnknownHostException;
+import java.util.Base64;
+import java.util.UUID;
+import com.esotericsoftware.kryo.io.Input;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"tr.edu.itu.bbf.cloudcore.distributed"})
@@ -20,6 +33,7 @@ public class Application implements CommandLineRunner {
 
     static final Logger logger = LoggerFactory.getLogger(Application.class);
 
+
     @Override
     public void run(String... args){
 
@@ -28,6 +42,8 @@ public class Application implements CommandLineRunner {
         try {
             String reply = worker.startCommunication();
             logger.info("********* Response from receiver = {}",reply);
+            StateMachineContext<States,Events> context = worker.deserializeStateMachineContext(reply);
+            logger.info("********* Deserialize context = {}",context.getState().toString());
         } catch (UnknownHostException e) {
             e.printStackTrace();
         }
--- src/main/java/tr/edu/itu/bbf/cloudcore/distributed/service/StateMachineWorker.java
+++ src/main/java/tr/edu/itu/bbf/cloudcore/distributed/service/StateMachineWorker.java
@@ -1,19 +1,30 @@
 package tr.edu.itu.bbf.cloudcore.distributed.service;
 
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.io.Input;
+import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.messaging.MessageHeaders;
 import org.springframework.statemachine.StateMachine;
+import org.springframework.statemachine.StateMachineContext;
+import org.springframework.statemachine.kryo.MessageHeadersSerializer;
+import org.springframework.statemachine.kryo.StateMachineContextSerializer;
+import org.springframework.statemachine.kryo.UUIDSerializer;
 import org.springframework.stereotype.Service;
 import tr.edu.itu.bbf.cloudcore.distributed.entity.Events;
 import tr.edu.itu.bbf.cloudcore.distributed.entity.States;
 import tr.edu.itu.bbf.cloudcore.distributed.ipc.CkptMessage;
 
 import javax.annotation.PostConstruct;
+import java.io.ByteArrayInputStream;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.Base64;
+import java.util.UUID;
 
 @Service
 public class StateMachineWorker {
@@ -32,6 +43,20 @@ public class StateMachineWorker {
 
     @Value("${CKPT_EXCHANGE_SMOC3}")
     private String CKPT_EXCHANGE_SMOC3;
+
+    private static final ThreadLocal<Kryo> kryoThreadLocal = new ThreadLocal<Kryo>() {
+        @NotNull
+        @SuppressWarnings("rawtypes")
+        @Override
+        protected Kryo initialValue() {
+            Kryo kryo = new Kryo();
+            kryo.addDefaultSerializer(StateMachineContext.class, new StateMachineContextSerializer());
+            kryo.addDefaultSerializer(MessageHeaders.class, new MessageHeadersSerializer());
+            kryo.addDefaultSerializer(UUID.class, new UUIDSerializer());
+            return kryo;
+        }
+    };
+
 
     static final Logger logger = LoggerFactory.getLogger(StateMachineWorker.class);
 
@@ -58,5 +83,14 @@ public class StateMachineWorker {
         String reply = (String) rabbitTemplate.convertSendAndReceive(CKPT_EXCHANGE_SMOC1,"rpc",msg);
         logger.info("********* Response from receiver = {}",reply);
         return reply;
+    }
+
+    public StateMachineContext<States, Events> deserializeStateMachineContext(String reply){
+        Kryo kryo = kryoThreadLocal.get();
+        //Base64.getEncoder().encodeToString(baos.toByteArray());
+        byte[] decodedString = Base64.getDecoder().decode(reply);
+        ByteArrayInputStream in = new ByteArrayInputStream(decodedString);
+        Input input = new Input(in);
+        return kryo.readObject(input, StateMachineContext.class);
     }
 }
