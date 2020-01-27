e00c4cfd2dc6ef34ec40bdb35f7d5ac013afdc43
celikelozdinc <ozdinc.celikel@gmail.com>
2020-01-27 21:44:40 +0300

    add memory reporting tool

---
+++ newclient/memory_report.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Learn PID
+echo "Getting PID ..."
+current_pid=$(grep "PID" log | awk -F "PID@HOSTNAME is " '{print $2}') | cut -d'@' -f1
+echo "PID is ""current_pid"
+
+# Memory Report from /proc/<pid>/status
+# xargs will do trimming
+echo "Querying /proc/pid/status ..."
+VmSize=$(grep "VmSize" /proc/"$current_pid"/status | awk -F 'VmPeak:|kB' '{print $2}' | xargs)
+VmPeak=$(grep "VmPeak" /proc/"$current_pid"/status | awk -F 'VmPeak:|kB' '{print $2}' | xargs)
+echo "VmSize is ""$VmSize"
+echo "VmPeak is ""$VmPeak"
+
+# Memory Report from top command
+# -v for awk : define variable
+# -n for top : exit after n iteration
+echo "Querying top ..."
+top -n 1 | awk -v src="$current_pid" '$1 == search {print $5}' | cut -d'm' -f1
