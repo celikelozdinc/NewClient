c58367f5f5e922e9731596ced5fb6ffe7794c2e4
celikelozdinc <ozdinc.celikel@gmail.com>
2020-01-30 08:02:02 +0300

    #smocs=3 for #events=9600

--- src/main/java/tr/edu/itu/bbf/cloudcore/distributed/ipc/RabbitConfig.java
+++ src/main/java/tr/edu/itu/bbf/cloudcore/distributed/ipc/RabbitConfig.java
@@ -17,7 +17,8 @@ public class RabbitConfig {
     @Value("${CKPT_EXCHANGE_SMOC3}")
     private String CKPT_EXCHANGE_SMOC3;
 
-   @Value("${CKPT_EXCHANGE_SMOC4}")
+    /*
+    @Value("${CKPT_EXCHANGE_SMOC4}")
     private String CKPT_EXCHANGE_SMOC4;
 
     @Value("${CKPT_EXCHANGE_SMOC5}")
@@ -41,7 +42,6 @@ public class RabbitConfig {
     @Value("${CKPT_EXCHANGE_SMOC11}")
     private String CKPT_EXCHANGE_SMOC11;
 
-    /*
     @Value("${CKPT_EXCHANGE_SMOC12}")
     private String CKPT_EXCHANGE_SMOC12;
 
@@ -63,6 +63,7 @@ public class RabbitConfig {
     @Bean
     DirectExchange smoc3_Ckpt_Exchange() { return new DirectExchange(CKPT_EXCHANGE_SMOC3); }
 
+    /*
     @Bean
     DirectExchange smoc4_Ckpt_Exchange() { return new DirectExchange(CKPT_EXCHANGE_SMOC4); }
 
@@ -87,7 +88,6 @@ public class RabbitConfig {
     @Bean
     DirectExchange smoc11_Ckpt_Exchange() { return new DirectExchange(CKPT_EXCHANGE_SMOC11); }
 
-     /*
     @Bean
     DirectExchange smoc12_Ckpt_Exchange() { return new DirectExchange(CKPT_EXCHANGE_SMOC12); }
 
